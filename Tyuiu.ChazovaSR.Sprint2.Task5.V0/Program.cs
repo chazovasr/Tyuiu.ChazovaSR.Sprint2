0bfd2ca6980b3cb7ae236a866137193c3c0fed97
Tyuiu.ChazovaSR@outlook.com <chazovas0@gmail.com>
2023-10-09 11:34:06 +0500

    Task5

---
+++ Tyuiu.ChazovaSR.Sprint2.Task5.V0/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.ChazovaSR.Sprint2.Task5.V0.Lib;
+namespace Tyuiu.ChazovaSR.Sprint2.Task5.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #2 | Выполнила: Чазова С.Р | ИСТНб-23-1";
+            Console.WriteLine("***********************************************************");
+            Console.WriteLine("* Спринт #2                                               *");
+            Console.WriteLine("* Тема: Оператор switch                                   *");
+            Console.WriteLine("* Задание #5                                              *");
+            Console.WriteLine("* Вариант #0                                              *");
+            Console.WriteLine("* Выполнила: Чазова Софья Романовна | ИСТНб-23-1          *");
+            Console.WriteLine("***********************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                 ");
+            Console.WriteLine("***********************************************************");
+            Console.WriteLine("*Написать программу, которая по данному номеру месяца     *");
+            Console.WriteLine("*определяет его название.                                 *");
+
+            Console.WriteLine("***********************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                        *");
+            Console.WriteLine("***********************************************************");
+
+            Console.WriteLine("Введите номер месяца : ");
+            int numMouth = Convert.ToInt32(Console.ReadLine());
+
+            string res;
+            if ((numMouth<1) || (numMouth>12))
+            {
+                res = "Введено неверное значение!";
+            }
+            else
+            {
+                res = "Это месяц: "+ds.FindMonthName(numMouth);
+            }
+
+
+
+            Console.WriteLine("***********************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                              *");
+            Console.WriteLine("***********************************************************");
+
+            Console.WriteLine(res);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Tyuiu.ChazovaSR.Sprint2.Task5.V3/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.ChazovaSR.Sprint2.Task5.V3.Lib;
+
+namespace Tyuiu.ChazovaSR.Sprint2.Task5.V3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #2 | Выполнила: Чазова С.Р | ИСТНб-23-1";
+            Console.WriteLine("***********************************************************");
+            Console.WriteLine("* Спринт #2                                               *");
+            Console.WriteLine("* Тема: Оператор switch                                   *");
+            Console.WriteLine("* Задание #5                                              *");
+            Console.WriteLine("* Вариант #3                                              *");
+            Console.WriteLine("* Выполнила: Чазова Софья Романовна | ИСТНб-23-1          *");
+            Console.WriteLine("***********************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                 ");
+            Console.WriteLine("***********************************************************");
+            Console.WriteLine("*Написать программу, которая в зависимости от порядкового *");
+            Console.WriteLine("*номера дня недели (1, 2, …, 7) выводит на экран его      *");
+            Console.WriteLine("*название (понедельник, вторник, …, воскресенье).         *");
+            Console.WriteLine("***********************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                        *");
+            Console.WriteLine("***********************************************************");
+
+            Console.WriteLine("Введите номер дня недели : ");
+            int numDay = Convert.ToInt32(Console.ReadLine());
+
+            string res;
+            if ((numDay < 1) || (numDay > 7))
+            {
+                res = "Введено неверное значение!";
+            }
+            else
+            {
+                res = "Это день недели: " + ds.FindDayName(numDay);
+            }
+
+
+
+            Console.WriteLine("***********************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                              *");
+            Console.WriteLine("***********************************************************");
+
+            Console.WriteLine(res);
+
+            Console.ReadKey();
+        }
+    }
+}
